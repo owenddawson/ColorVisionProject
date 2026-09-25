0dcad0c90e2f4393eaeaadc34043e2cc6853e2f4
owenddawson <owenddawson@outlook.com>
2026-09-25 16:10:10 -0700

    Add RGB color input

--- README.md
+++ README.md
@@ -1,27 +1,43 @@
-# Replace with your unique project name
+# RGB Color Comparison Tool
 
 ## Description
 
 **version 1.0**
 
-Replace with a description of *what* your program does (not *how* it works)
+This program allows the user to enter two colors using RGB values. Each color is stored as three values representing red, green, and blue. The program then displays both colors back to the user.
 
+This version is the first step toward creating a tool that can compare colors and determine how distinguishable they may be from each other.
 
 ## Developer
 
-Replace with your name
+Owen Dawson
 
 ## Example
 
-To run the program, give the following commands:
+To compile the program use the command:
 
+```bash
+g++ main.cpp -o main
 ```
-g++ --std=c++11 *.cpp -o cvp
-./cvp
+
+to run the program use the command:
+
+```bash
+./main
 ```
 
 Here is an example of the program running:
 
-```
-Replace this with a copy-pasted example of the input/output of your program running.
+```text
+Enter your R,G,B values for Color 1:
+R: 255
+G: 0
+B: 0
+Color 1 RGB: 255, 0, 0
+
+Enter your R,G,B values for Color 2:
+R: 0
+G: 255
+B: 0
+Color 2 RGB: 0, 255, 0
 ```
--- main.cpp
+++ main.cpp
@@ -1,8 +1,72 @@
 #include <iostream>
+#include <array>
 using namespace std;
 
 int main()
 {
+  using std::string;
+
+  array<int, 3> color1 = {0, 0, 0};
+  array<int, 3> color2 = {0, 0, 0};
+  array<string, 3> channels = {"R", "G", "B"};
+
+  cout << "Enter your R,G,B values for Color 1: " << endl;
+  for (int index = 0; index < 3; index++)
+  {
+      cout << channels[index] << ": ";
+      cin >> color1[index];
+  };
+
+  cout << "Color 1 RGB: "
+        << color1[0] << ", "
+        << color1[1] << ", "
+        << color1[2] << endl;
+
+  cout << "Enter your R,G,B values for Color 2: " << endl;
+  for (int index = 0; index < 3; index++)
+  {
+      cout << channels[index] << ": ";
+      cin >> color2[index];
+  };
+
+  cout << "Color 2 RGB: "
+        << color2[0] << ", "
+        << color2[1] << ", "
+        << color2[2] << endl;
+
+
+  /* OLD CODE
+  array<int, 3> color1 = {0, 0, 0};
+  array<int, 3> color2 = {0, 0, 0};
+
+  cout << "Enter R: ";
+  cin >> color1[0];
+
+  cout << "Enter G: ";
+  cin >> color1[1];
+
+  cout << "Enter B: ";
+  cin >> color1[2];
+
+  cout << "Color 1 RGB: "
+        << color1[0] << ", "
+        << color1[1] << ", "
+        << color1[2] << endl;
+
+  cout << "Enter R: ";
+  cin >> color2[0];
+
+  cout << "Enter G: ";
+  cin >> color2[1];
+
+  cout << "Enter B: ";
+  cin >> color2[2];
+
+  cout << "Color 2 RGB: "
+        << color2[0] << ", "
+        << color2[1] << ", "
+        << color2[2] << endl;
+  */
 
   return 0;
 }
